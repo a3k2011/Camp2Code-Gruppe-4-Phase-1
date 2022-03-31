6cd1348c47fac09bfc3b235977ab06550b126e50
timKoenig50 <timkoenig1990@yahoo.de>
2022-03-31 09:56:56 +0200

    Include class  Datenlogger

--- PiCar.py
+++ PiCar.py
@@ -1,11 +1,9 @@
 import basisklassen
-import time
-import json
+import os, json, time
+from datetime import datetime
 import numpy as np
-import datenlogger
 from concurrent import futures
 from concurrent.futures import ThreadPoolExecutor
-
 
 angle_from_sensor = {
     0: 100,
@@ -54,7 +52,7 @@ class BaseCar:
     def startDriveMode(self):
         self._active = True
         self.steering_angle = 0
-        self._dl = datenlogger.Datenlogger(log_file_path=self._log_file_path)
+        self._dl = Datenlogger(log_file_path=self._log_file_path)
         self._worker = ThreadPoolExecutor(max_workers=5)
         self._worker.submit(self.dlWorker)
 
@@ -384,6 +382,68 @@ class SensorCar(Sonic):
             print(self._ir_sensor_analog)
             print(self._ir_sensors)
             time.sleep(self.IF_FREQ)
+
+
+
+class Datenlogger:
+    """Datenlogger Klasse
+    speichert übergebene Tupels oder Listen mit Angabe des Zeitdeltas seid Start der Aufzeichnung in ein json-File
+    """
+
+    def __init__(self, log_file_path=None):
+        """Zielverzeichnis fuer Logfiles kann beim Init mit uebergeben werden
+            Wenn der Ordner nicht existiert wird er erzeugt
+
+        Args:
+            log_file_path (_type_, optional): Angabe des Zielordners. Defaults to None.
+        """
+        self._log_file = {}
+        self._log_data = []
+        self._start_timestamp = 0
+        self._logger_running = False
+        self._log_file_path = log_file_path
+
+    def start(self):
+        """starten des Loggers"""
+        print("Logger gestartet")
+        self._logger_running = True
+        self._start_timestamp = time.time()
+        self._log_file["start"] = str(datetime.now()).partition(".")[0]
+
+    def append(self, data):
+        """Daten an den Logger senden
+
+        Args:
+            data (list): ein Element (Liste) wird an den Logger uebergeben
+        """
+        if self._logger_running:
+            ts = round((time.time() - self._start_timestamp), 2)
+            self._log_data.append([ts] + data)
+
+    def save(self):
+        """speichert die uebergebenen Daten"""
+        if self._logger_running and (len(self._log_data) > 0):
+            self._logger_running = False
+            self._log_file["data"] = self._log_data
+            self._log_file["ende"] = str(datetime.now()).partition(".")[0]
+            filename = self._log_file.get("start").partition(".")[0]
+            filename = (
+                filename.replace("-", "").replace(":", "").replace(" ", "_")
+                + "_drive.log"
+            )
+            if self._log_file_path != None:
+                logfile = os.path.join(self._log_file_path, filename)
+                if not os.path.isdir(self._log_file_path):
+                    os.mkdir(self._log_file_path)
+            else:
+                logfile = filename
+            with open(logfile, "w") as f:
+                json.dump(self._log_data, f)
+            self._log_file.clear()
+            self._log_data.clear()
+            print("Log-File saved to:", logfile)
+
+
 
 def main():
     '''
