b4df77c90b248d28508ecca881f66c17b7bdcd4a
timKoenig50 <timkoenig1990@yahoo.de>
2022-03-31 09:45:06 +0200

    Docstrings

--- PiCar_Main.py
+++ PiCar_Main.py
@@ -1,21 +1,29 @@
 import PiCar
 
 def main():
-    #myCar = PiCar.BaseCar()
-    #myCar = PiCar.Sonic()
+    '''
+        Main-Programm: Manuelles Ansteuern der implementierten Fahrparcours 1-6
+
+        Args[Klassen]:
+                        PiCar.BaseCar()
+                        PiCar.Sonic()
+                        PiCar.SensorCar()
+        Funktionen der Klassen:
+                        fp1() - fp6()
+        Args[Fahrparcour]:
+                        v (int): Geschwindigkeit. Default 50
+    '''
     myCar = PiCar.SensorCar()
 
-    myCar.test_ir()
+    #myCar.fp1(v=50)
 
-    #myCar.fp1(50)
+    #myCar.fp2(v=50)
 
-    #myCar.fp2(50)
+    #myCar.fp3(v=50)
 
-    #myCar.fp3(50)
+    #myCar.fp4(v=50)
 
-    #myCar.fp4(75)
-
-    #myCar.fp5(50)
+    myCar.fp5(v=50)
 
 
 if __name__ == "__main__":
