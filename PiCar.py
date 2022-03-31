900aaa9bded3b490802359f3bcfc12c66ad1da6b
timKoenig50 <timkoenig1990@yahoo.de>
2022-03-31 09:49:25 +0200

    Include Main() Function

--- PiCar.py
+++ PiCar.py
@@ -384,3 +384,33 @@ class SensorCar(Sonic):
             print(self._ir_sensor_analog)
             print(self._ir_sensors)
             time.sleep(self.IF_FREQ)
+
+def main():
+    '''
+        Main-Programm: Manuelles Ansteuern der implementierten Fahrparcours 1-6
+
+        Args[Klassen]:
+                        PiCar.BaseCar()
+                        PiCar.Sonic()
+                        PiCar.SensorCar()
+        Funktionen der Klassen:
+                        fp1() - fp6()
+        Args[Fahrparcour]:
+                        v (int): Geschwindigkeit. Default 50
+    '''
+
+    myCar = SensorCar()
+
+    #myCar.fp1(v=50)
+
+    #myCar.fp2(v=50)
+
+    #myCar.fp3(v=50)
+
+    #myCar.fp4(v=50)
+
+    myCar.fp5(v=50)
+
+
+if __name__ == "__main__":
+    main()
